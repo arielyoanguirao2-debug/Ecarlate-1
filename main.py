5e7dcc9366c4ea738fc9db9491189b076d2718ff
arielyoanguirao2-debug <arielyoanguirao2@gmail.com>
2026-03-25 16:35:03 +0000

    Implement URL testing functionality
    Added a URL testing function that checks the status of a website and prints appropriate messages based on the response code.

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-def main():
-    print("Hello, World!")
+import requests
+import time
+print("bonjour")
 
-if __name__ == '__main__':
-    main()
+def test():
+   url=input("entrez l'URL a tester : ")
+   while True:
+    requete = requests.get(url, timeout=10)
+    if requete.status_code==200:
+       print("le site est en ligne ")
+       print()
+    elif requete.status_code!=200:
+       if requete.status_code==404:
+           print("le site est hors ligne")
+           print()
+           print("le site est introuvable ")
+       elif requete.status_code==403:
+           print("le serveur est en ligne")
+           print()
+           print("mais refuse de repondre")
+    time.sleep(5)
+    
+try :
+    test()           
+except ValueError:
+   print("entrez un adresse valide")
+   print("relancer le programme et recommencer ")
+   
+   
+   
+    
