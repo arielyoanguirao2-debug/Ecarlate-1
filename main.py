b801a093a071b10e43589302670cd305d402668d
arielyoanguirao2-debug <arielyoanguirao2@gmail.com>
2026-03-28 01:42:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 import requests
 import time
-print("bonjour")
+def test_unique():
+     url=input("entrez l'URL a tester : ")
+     requete = requests.get(url)
+     
+     if requete.status_code==200:
+         print("le site est en ligne")
+         
+     elif requete.status_code!=200:
+        
+        if requete.status_code==404:
+           
+           print("le site est hors ligne")
+           print()
+           print("le site est introuvable ")
+           
+        elif requete.status_code==403:
+           print("le serveur est en ligne")
+           print()
+           print("mais refuse de repondre")
 
-def test():
+def test_multiple():
    url=input("entrez l'URL a tester : ")
+   print("try :
+    test()           
+except ValueError:
+   print("entrez un adresse valide")
+   print("relancer le programme et recommencer ")
+except requests.exceptions.RequestException:
+   print("vous êtes hors ligne")
+   print("connecter vous et ressayer")
+")
    while True:
     requete = requests.get(url, timeout=10)
     if requete.status_code==200:
@@ -19,17 +46,37 @@ def test():
            print()
            print("mais refuse de repondre")
     time.sleep(5)
-    
-try :
-    test()           
-except ValueError:
+
+
+
+print("1-verification unique")
+print("2-verification continue")
+print("")
+choix=int(input("entrez votre choix : "))
+if choix==1:
+    try :
+       test_unique()      
+    except ValueError:
+       print("entrez un adresse valide")
+       print("relancer le programme et recommencer ")
+    except requests.exceptions.RequestException:
+       print("vous êtes hors ligne")
+       print("connecter vous et ressayer")
+
+elif choix==2:
+ try :
+    test_multiple()           
+ except ValueError:
    print("entrez un adresse valide")
    print("relancer le programme et recommencer ")
-except requests.exceptions.RequestException:
+ except requests.exceptions.RequestException:
    print("vous êtes hors ligne")
    print("connecter vous et ressayer")
-   
-   
+       
+else:
+    print("relancer le programme et suivez les indications ")
+
+
    
     
    
